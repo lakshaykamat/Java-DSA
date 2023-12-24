6d2cecc9e3bbf5b9cc5a2e331c2ed7e5254f5b5f
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-24 23:31:12 +0530

    renamed

--- Array/src/test/java/org/example/RunningSumTest.java
+++ Array/src/test/java/org/example/RunningSumTest.java
@@ -11,7 +11,7 @@ class RunningSumTest {
         int[] input = {1,2,3,4};
         int[] ans = {1,3,6,10};
         assertArrayEquals(ans,LeetCode.runningSum(input));
-        assertArrayEquals(ans,LeetCode.runningSum2(input));
+        //assertArrayEquals(ans,LeetCode.runningSum2(input));
     }
 
     @Test
@@ -19,13 +19,11 @@ class RunningSumTest {
         int[] input = {3,1,2,10,1};
         int[] ans = {3,4,6,16,17};
         assertArrayEquals(ans,LeetCode.runningSum(input));
-        assertArrayEquals(ans,LeetCode.runningSum2(input));
     }
     @Test
     void Test3() {
         int[] input = {1,1,1,1,1};
         int[] ans = {1,2,3,4,5};
         assertArrayEquals(ans,LeetCode.runningSum(input));
-        assertArrayEquals(ans,LeetCode.runningSum2(input));
     }
 }
