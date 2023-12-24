d3f02bdf04121f1f93c0545152fd90df235125a1
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-24 15:09:04 +0530

    new array question solved
    - running sum
    - maximum wealth
    - shuffle
    - numIdenticalPairs

--- Array/src/LeetCode.java
+++ Array/src/LeetCode.java
@@ -21,5 +21,61 @@ public class LeetCode {
         }
         return ans;
     }
+<<<<<<< refs/remotes/origin/master
 >>>>>>> concatenation of array
+=======
+    public static int[] runningSum(int[] nums) {
+        //https://leetcode.com/problems/running-sum-of-1d-array/
+        int[] ans = new int[nums.length];
+        for(int i=1;i<nums.length;i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+    public static int[] runningSum2(int[] nums) {
+        //https://leetcode.com/problems/running-sum-of-1d-array/
+        int[] ans = new int[nums.length];
+        if(nums.length == 0){
+            return ans;
+        }
+        ans[0] = nums[0];
+        for(int i=1;i<nums.length;i++) {
+            ans[i] = nums[i]+ans[i-1];
+        }
+        return ans;
+    }
+    public static int maximumWealth(int[][] accounts) {
+        //https://leetcode.com/problems/richest-customer-wealth/
+        int maxWeath = 0;
+        for(int i=0;i<accounts.length;i++){
+            int currentWealth = 0;
+            for(int j=0;j<accounts[i].length;j++){
+                currentWealth += accounts[i][j];
+            }
+            maxWeath = Integer.max(maxWeath,currentWealth);
+        }
+        return maxWeath;
+    }
+    public static int[] shuffle(int[] nums, int n) {
+        //https://leetcode.com/problems/shuffle-the-array/description/
+        //Under review
+        int[] newArr = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+
+        }
+        return newArr;
+    }
+    public static int numIdenticalPairs(int[] nums) {
+        //https://leetcode.com/problems/number-of-good-pairs/description/
+        int countPair = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if((nums[j] == nums[i]) && (i < j)){
+                    countPair++;
+                }
+            }
+        }
+        return countPair;
+    }
+>>>>>>> new array question solved
 }
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< refs/remotes/origin/master
         int[] array = {1,2,3};
         int[][] arr1 = {
                 {1,2,3},
@@ -15,6 +17,21 @@ public class Main {
         //System.out.println(Arrays.toString(n));
         System.out.println(Arrays.toString(LeetCode.buildArray(new int[]{0, 2, 1, 5, 3, 4})));
 >>>>>>> concatenation of array
+=======
+        //Concatenation of Array: https://leetcode.com/problems/concatenation-of-array/
+        //System.out.println(Arrays.toString(LeetCode.getConcatenation(new int[]{1, 2, 3})));
+>>>>>>> new array question solved
 
+        //Build Array from Permutation: https://leetcode.com/problems/build-array-from-permutation/
+        //System.out.println(Arrays.toString(LeetCode.buildArray(new int[]{0, 2, 1, 5, 3, 4})));
+
+        //Running sum of 1D array: https://leetcode.com/problems/running-sum-of-1d-array/
+        //System.out.println(Arrays.toString(LeetCode.runningSum2((new int[]{1, 2, 3, 4}))));
+
+        //Rich Customer Wealth: https://leetcode.com/problems/richest-customer-wealth/
+        //System.out.println(LeetCode.maximumWealth(new int[][]{{1,5},{7,3},{3,5}}));
+
+        //Number of Good Pairs: https://leetcode.com/problems/number-of-good-pairs/
+        //System.out.println(LeetCode.numIdenticalPairs(new int[]{1,2,3}));
     }
 }
