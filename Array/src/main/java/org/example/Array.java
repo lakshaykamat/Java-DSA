7aeeed8467b1d28ba745a51689646292d38bb63d
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-25 19:44:55 +0530

    some array function created
    - swap values
    - maximum array value
    - maximum array value index

--- Array/src/main/java/org/example/Array.java
+++ Array/src/main/java/org/example/Array.java
@@ -26,4 +26,33 @@ public class Array {
         }
         return count;
     }
+    static int[] swap(int[] arr, int index1, int index2){
+        if(index1>=arr.length-1 || index2>=arr.length-1){
+            System.out.println("Index can't be greater than array");
+            return arr;
+        }
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+        return arr;
+    }
+    static int maximumValue(int[] arr){
+        int max = arr[0];
+        for(int item:arr){
+            max = Integer.max(max,item);
+        }
+        return max;
+    }
+    static int maximumValueIndex(int[] arr){
+        int max = arr[0];
+        int index = 0;
+        for(int item:arr){
+            max = Integer.max(max,item);
+        }
+        for(int item:arr){
+            if(item == max) break;
+            index++;
+        }
+        return index;
+    }
 }
