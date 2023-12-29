995057787c6a7225156f6503c8a59d80bb862393
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-30 00:44:50 +0530

    Add Extra functions

--- Array/src/main/java/org/example/Array.java
+++ Array/src/main/java/org/example/Array.java
@@ -71,4 +71,83 @@ public class Array {
         }
         return arr;
     }
+    static int countOddNumberInMatrix(int[][] matrix){
+        int ans = 0;
+        for(int row=0;row<matrix.length;row++){
+            for(int col=0;col<matrix[0].length;col++){
+                if(matrix[row][col] % 2 != 0){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    static void incrementCol(int[][] matrix, int col, int value) {
+        //Increment all column value
+
+        //check given column is existing or not
+        if (col > matrix[0].length - 1) {
+            System.out.println("Col don't exist");
+            return;
+        }
+
+        //finding that column
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (j == col){
+                    matrix[i][j] = matrix[i][j] + value;//incrementing
+                }
+            }
+        }
+    }
+
+    static void incrementRow(int[][] matrix, int row, int value) {
+        //Increment all row value value
+
+        //check given column is existing or not
+        if (row > matrix.length - 1) {
+            System.out.println("Row don't exist");
+            return;
+        }
+
+        //finding that row
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == row) {
+                    matrix[i][j] = matrix[i][j] + value; //incrementing
+                }
+            }
+        }
+    }
+    static boolean isMatrixEqual(int[][] mat1, int[][] mat2) {
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1.length; j++) {
+                if (mat1[i][j] != mat2[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    static void rotateMatrixClockwise(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        int start = 0, end = matrix.length - 1;
+        while (start <= end) {
+            for (int i = 0; i < matrix.length; i++) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+            }
+            start++;
+            end--;
+        }
+    }
 }
